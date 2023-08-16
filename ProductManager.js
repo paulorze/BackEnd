166df8b54c92d9a8b314b62435e5b1e476a325dd
pauloRzeszut <agustinrzeszut@hotmail.com>
2023-08-16 15:08:15 -0300

    feature: added persistence in files

--- ProductManager.js
+++ ProductManager.js
@@ -1,10 +1,21 @@
+const fs = require('fs/promises');
+
 class ProductManager {
-    #products;
-    constructor (){
+    #products
+    constructor () {
         this.#products = [];
     };
 
-    addProduct ({title, description, price, thumbnail, code, stock}) {
+    async init() {
+        try {
+            const productsData = await fs.readFile('./products.js', 'UTF-8');
+            this.#products = JSON.parse(productsData);
+        } catch {
+            console.log('No se pudieron obtener todos los productos. Se trabajara como si no existiera ninguno');
+        };
+    };
+
+    async addProduct ({title, description, price, thumbnail, code, stock}) {
         const existingCode = this.#products.find(product => product.code === code);
         if (existingCode) {
             console.log('El codigo ingresado ya fue utilizado en otro producto.');
@@ -24,11 +35,119 @@ class ProductManager {
             stock
         };
         this.#products.push(product);
+        try {
+            await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+            console.log('Producto agregado exitosamente.');
+        } catch {
+            console.log('Error al cargar el producto.');
+        };
         return product;
     };
 
+    async updateProduct(id, key, value) {
+        const foundProduct = this.getProductByID(id)
+        if (!foundProduct) return;
+        switch (key) {
+            case 'title':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, title : value}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Titulo del producto modificado de manera exitosa.')
+                break;
+            case 'description':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, description : value}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Descripcion del producto modificada de manera exitosa.')
+                break;
+            case 'price':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, price : value}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Precio del producto modificado de manera exitosa.')
+                break;
+            case 'thumbnail':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, thumbnail : value}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Miniatura del producto modificada de manera exitosa.')
+                break;
+            case 'code':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, code : value}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Codigo del producto modificado de manera exitosa.')
+                break;
+            case 'stock':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, stock : value}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Stock del producto modificado de manera exitosa.')
+                break;
+            case 'all':
+                this.#products = this.#products.map(product =>{
+                    if (product.id === foundProduct.id) {
+                        return {...product, 
+                            title: value.title,
+                            description: value.description,
+                            price: value.price,
+                            thumbnail: value.thumbnail,
+                            code: value.code,
+                            stock : value.stock}
+                    } else {
+                        return product
+                    };
+                });
+                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+                console.log('Informacion del producto modificada de manera exitosa.')
+                break;
+            default:
+                console.log('Por favor, ingrese un parametro a modificar valido.')
+                break;
+        }
+    };
+
+    async deleteProduct(id){
+        const foundProduct = this.getProductByID(id)
+        if (!foundProduct) return;
+        this.#products = this.#products.filter(product => product.id !== foundProduct.id)
+        await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+        console.log('Producto eliminado exitosamente.');
+    };
+
     getProducts () {
-        return this.#products
+        const productsData = this.#products;
+        return productsData;
     };
 
     getProductByID (productID) {
@@ -37,60 +156,62 @@ class ProductManager {
             console.log(`NOT FOUND: El producto con ID ${productID} no existe.`);
             return;
         } else {
-            console.log(`El producto encontrado con el ID ${productID} es "${productFound.title}", de codigo "${productFound.code}". Su descripcion es "${productFound.description}" y su miniatura, ${productFound.thumbnail}. Tiene un costo de $${productFound.price} y un stock de ${productFound.stock} unidad(es).`);
             return productFound;
         }
     };
 };
 
-const productManager = new ProductManager();
-const product1 = productManager.addProduct({
-    title : 'Producto 1',
-    description : 'Producto nuevo y muy piola',
-    price : 200,
-    thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-    code: 'P001',
-    stock: 20
-});
-const product2 = productManager.addProduct({
-    title : 'Producto 2',
-    description : 'Producto nuevo y muy piola',
-    price : 200,
-    thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-    code: 'P002',
-    stock: 20
-});
-const product3 = productManager.addProduct({
-    title : 'Producto 3',
-    description : 'Producto nuevo y muy piola',
-    price : 200,
-    thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-    code: 'P003',
-    stock: 20
-});
-const product4 = productManager.addProduct({
-    title : 'Producto 4',
-    description : 'Producto nuevo y muy piola',
-    price : 200,
-    thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-    code: 'P004',
-    stock: 20
-});
-const product5 = productManager.addProduct({
-    title : 'Producto 5',
-    description : 'Producto nuevo y muy piola',
-    price : 200,
-    thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-    code: 'P001',
-    stock: 20
-});
-const product6 = productManager.addProduct({
-    title : 'Producto 6',
-    description : 'Producto nuevo y muy piola',
-    thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-    code: 'P006'
-});
-const allProducts = productManager.getProducts();
-console.log('Todos los productos son: ', allProducts);
-const busqueda1 = productManager.getProductByID(1);
-const busqueda2 = productManager.getProductByID(10);
+
+
+(async () =>{
+    const productManager = new ProductManager();
+    await productManager.init();
+    await productManager.addProduct({
+        title : 'Producto 5',
+        description : 'Producto nuevo y muy piola',
+        price : 200,
+        thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
+        code: 'P001',
+        stock: 20
+    });
+    await productManager.addProduct({
+        title : 'Producto 5',
+        description : 'Producto nuevo y muy piola',
+        thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
+        code: 'P005'
+    });
+    await productManager.addProduct({
+        title : 'Producto 5',
+        description : 'Producto nuevo y muy piola',
+        price : 200,
+        thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
+        code: 'P005',
+        stock: 20
+    });
+    let productsList = productManager.getProducts();
+    console.log(productsList);
+    const productoEncontradoFallido = productManager.getProductByID(10);
+    const productoEncontradoExitoso  = productManager.getProductByID(4);
+    console.log(productoEncontradoExitoso);
+    await productManager.deleteProduct(4);
+    productsList = productManager.getProducts();
+    console.log(productsList);
+    await productManager.updateProduct(3, 'asd', 'OTRO TITULO');
+    await productManager.updateProduct(3, 'title', 'OTRO TITULO');
+    await productManager.updateProduct(3, 'description', 'UNA COSA RE LOCA Y COPADA');
+    await productManager.updateProduct(3, 'price', 999999);
+    await productManager.updateProduct(3, 'code', 'P9999');
+    await productManager.updateProduct(3, 'stock', 9999999);
+    productsList = productManager.getProducts();
+    console.log(productsList);
+    await productManager.updateProduct(3, 'all', {
+        title: "Producto 4",
+        description: "Producto nuevo y muy piola",
+        price: 200,
+        thumbnail: "https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png",
+        code: "P004",
+        stock: 20
+    });
+    productsList = productManager.getProducts();
+    console.log(productsList);
+})();
---
+++ products.js
@@ -0,0 +1,38 @@
+[
+    {
+        "id": 0,
+        "title": "Producto 1",
+        "description": "Producto nuevo y muy piola",
+        "price": 200,
+        "thumbnail": "https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png",
+        "code": "P001",
+        "stock": 20
+    },
+    {
+        "id": 1,
+        "title": "Producto 2",
+        "description": "Producto nuevo y muy piola",
+        "price": 200,
+        "thumbnail": "https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png",
+        "code": "P002",
+        "stock": 20
+    },
+    {
+        "id": 2,
+        "title": "Producto 3",
+        "description": "Producto nuevo y muy piola",
+        "price": 200,
+        "thumbnail": "https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png",
+        "code": "P003",
+        "stock": 20
+    },
+    {
+        "id": 3,
+        "title": "Producto 4",
+        "description": "Producto nuevo y muy piola",
+        "price": 200,
+        "thumbnail": "https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png",
+        "code": "P004",
+        "stock": 20
+    }
+]
