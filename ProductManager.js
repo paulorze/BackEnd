5d10769118a25618b36ab097a1bb54d179801d4c
pauloRzeszut <agustinrzeszut@hotmail.com>
2023-08-23 09:46:51 -0300

    2da entrega, reentrega incluyendo this.path y refactorizacion del switch case en updateProduct()

--- ProductManager.js
+++ ProductManager.js
@@ -4,11 +4,12 @@ class ProductManager {
     #products
     constructor () {
         this.#products = [];
+        this.path = './products.json';
     };
 
     async init() {
         try {
-            const productsData = await fs.readFile('./products.js', 'UTF-8');
+            const productsData = await fs.readFile(this.path, 'UTF-8');
             this.#products = JSON.parse(productsData);
         } catch {
             console.log('No se pudieron obtener todos los productos. Se trabajara como si no existiera ninguno');
@@ -36,7 +37,7 @@ class ProductManager {
         };
         this.#products.push(product);
         try {
-            await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+            await fs.writeFile(this.path, JSON.stringify(this.#products, null, 4), 'UTF-8');
             console.log('Producto agregado exitosamente.');
         } catch {
             console.log('Error al cargar el producto.');
@@ -47,101 +48,38 @@ class ProductManager {
     async updateProduct(id, key, value) {
         const foundProduct = this.getProductByID(id)
         if (!foundProduct) return;
-        switch (key) {
-            case 'title':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, title : value}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Titulo del producto modificado de manera exitosa.')
-                break;
-            case 'description':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, description : value}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Descripcion del producto modificada de manera exitosa.')
-                break;
-            case 'price':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, price : value}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Precio del producto modificado de manera exitosa.')
-                break;
-            case 'thumbnail':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, thumbnail : value}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Miniatura del producto modificada de manera exitosa.')
-                break;
-            case 'code':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, code : value}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Codigo del producto modificado de manera exitosa.')
-                break;
-            case 'stock':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, stock : value}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Stock del producto modificado de manera exitosa.')
-                break;
-            case 'all':
-                this.#products = this.#products.map(product =>{
-                    if (product.id === foundProduct.id) {
-                        return {...product, 
-                            title: value.title,
-                            description: value.description,
-                            price: value.price,
-                            thumbnail: value.thumbnail,
-                            code: value.code,
-                            stock : value.stock}
-                    } else {
-                        return product
-                    };
-                });
-                await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
-                console.log('Informacion del producto modificada de manera exitosa.')
-                break;
-            default:
-                console.log('Por favor, ingrese un parametro a modificar valido.')
-                break;
-        }
+        const validKeys = ['all', 'title', 'description', 'price', 'thumbnai', 'code', 'stock']
+        if (!validKeys.includes(key)) return console.log('Por favor, ingrese un parametro valido.');
+        if(key === 'all') {
+            this.#products = this.#products.map(product =>{
+                if (product.id === foundProduct.id) {
+                    return {...product, 
+                        title: value.title,
+                        description: value.description,
+                        price: value.price,
+                        thumbnail: value.thumbnail,
+                        code: value.code,
+                        stock : value.stock}
+                } else {
+                    return product
+                };
+            });
+        } else{
+            this.#products = this.#products.map(product =>{
+                if (product.id === foundProduct.id) {
+                    product[key] = value
+                }
+                return product 
+            });
+        };
+        await fs.writeFile(this.path, JSON.stringify(this.#products, null, 4), 'UTF-8');
     };
 
     async deleteProduct(id){
         const foundProduct = this.getProductByID(id)
         if (!foundProduct) return;
         this.#products = this.#products.filter(product => product.id !== foundProduct.id)
-        await fs.writeFile('./products.js', JSON.stringify(this.#products, null, 4), 'UTF-8');
+        await fs.writeFile(this.path, JSON.stringify(this.#products, null, 4), 'UTF-8');
         console.log('Producto eliminado exitosamente.');
     };
 
@@ -181,35 +119,35 @@ class ProductManager {
         code: 'P005'
     });
     await productManager.addProduct({
-        title : 'Producto 5',
+        title : 'Producto 11',
         description : 'Producto nuevo y muy piola',
         price : 200,
         thumbnail: 'https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png',
-        code: 'P005',
+        code: 'P011',
         stock: 20
     });
     let productsList = productManager.getProducts();
     console.log(productsList);
-    const productoEncontradoFallido = productManager.getProductByID(10);
-    const productoEncontradoExitoso  = productManager.getProductByID(4);
+    const productoEncontradoFallido = productManager.getProductByID(12);
+    const productoEncontradoExitoso  = productManager.getProductByID(10);
     console.log(productoEncontradoExitoso);
-    await productManager.deleteProduct(4);
+    await productManager.deleteProduct(10);
     productsList = productManager.getProducts();
     console.log(productsList);
-    await productManager.updateProduct(3, 'asd', 'OTRO TITULO');
-    await productManager.updateProduct(3, 'title', 'OTRO TITULO');
-    await productManager.updateProduct(3, 'description', 'UNA COSA RE LOCA Y COPADA');
-    await productManager.updateProduct(3, 'price', 999999);
-    await productManager.updateProduct(3, 'code', 'P9999');
-    await productManager.updateProduct(3, 'stock', 9999999);
+    await productManager.updateProduct(9, 'asd', 'OTRO TITULO');
+    await productManager.updateProduct(9, 'title', 'OTRO TITULO');
+    await productManager.updateProduct(9, 'description', 'UNA COSA RE LOCA Y COPADA');
+    await productManager.updateProduct(9, 'price', 999999);
+    await productManager.updateProduct(9, 'code', 'P9999');
+    await productManager.updateProduct(9, 'stock', 9999999);
     productsList = productManager.getProducts();
     console.log(productsList);
-    await productManager.updateProduct(3, 'all', {
-        title: "Producto 4",
+    await productManager.updateProduct(9, 'all', {
+        title: "Producto 10",
         description: "Producto nuevo y muy piola",
         price: 200,
         thumbnail: "https://w7.pngwing.com/pngs/405/993/png-transparent-pikachu-haunter-drawing-pokxe9mon-art-tongue-purple-monster-purple-mammal-cat-like-mammal.png",
-        code: "P004",
+        code: "P010",
         stock: 20
     });
     productsList = productManager.getProducts();
